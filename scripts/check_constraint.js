4a8e8d5a8ecc8c763ae4279d56e787084a6c82cd
Yahya <yaboula@alumno.upv.es>
2026-06-03 00:43:08 +0200

    chore: ignore scripts folder

--- scripts/check_constraint.js
+++
@@ -1,4 +0,0 @@
-const pool = require('./src/config/database');
-pool.query("SELECT pg_get_constraintdef(oid) FROM pg_constraint WHERE conname = 'chk_seccion'")
-  .then(r => console.log(r.rows))
-  .finally(() => pool.end());
